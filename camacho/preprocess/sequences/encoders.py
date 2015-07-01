3413c834a3d1104bf41cc0d0d5de2f79d5cc7977
James Knighton <iamknighton@gmail.com>
2015-07-01 12:55:09 -0700

    Remove oov_token, as there is no inverse_transform().

--- camacho/preprocess/sequences/encoders.py
+++ camacho/preprocess/sequences/encoders.py
@@ -18,10 +18,9 @@ def assign_ints_by_freq(a2count):
 
 
 class StandardEncoder(TransformerMixin):
-    def __init__(self, min_freq=50, oov_int=0, oov_token=' '):
+    def __init__(self, min_freq=50, oov_int=0):
         self._min_freq = min_freq
         self._oov_int = oov_int
-        self._oov_token = oov_token
 
     def fit(self, aaa):
         # Count tokens.
