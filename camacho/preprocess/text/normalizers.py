a6f2661b9c87e4009631065f78a1c1cb9eb84c7d
James Knighton <iamknighton@gmail.com>
2015-06-30 17:54:12 -0700

    AggressiveUnicodeNormalizer.

--- camacho/preprocess/text/normalizers.py
+++ camacho/preprocess/text/normalizers.py
@@ -14,7 +14,8 @@ class LowerCaseNormalizer(TransformerMixin):
 
 
 class UnicodeNormalizer(TransformerMixin):
-    def __init__(self, form='nfc'):
+    def __init__(self, form='NFC'):
+        assert form in ('NFC', 'NFD', 'NFKC', 'NFKD')
         self._form = form
 
     def fit(self, texts):
@@ -27,12 +28,70 @@ class UnicodeNormalizer(TransformerMixin):
         return map(lambda s: unicodedata.normalize(self._form, s), texts)
 
 
+def aggressively_normalize(text):
+    nn = map(unicodedata.combining, text)
+
+    tmp = []
+    nnn_ccc = []
+    for c, n in zip(text, nn):
+        if n:
+            tmp.append((n, c))
+            continue
+        if tmp:
+            nnn_ccc.append(tmp)
+        tmp = [(n, c)]
+    if tmp:
+        nnn_ccc.append(tmp)
+
+    ss = []
+    for nn_cc in nnn_ccc:
+        nn_cc.sort()
+        s = ''.join(map(lambda n, c: c, nn_cc))
+        ss.append(s)
+
+    return ''.join(ss)
+
+
+class AggressiveUnicodeNormalizer(TransformerMixin):
+    """
+    Normalize a bit further than the standard Unicode normalization forms.
+
+    Decomposes, reorders segments, then recomposes.  Drop-in replacement for
+    UnicodeNormalizer.
+    """
+
+    def __init__(self, before='NFD', after='NFC'):
+        assert before in (None, 'NFD', 'NFKD')
+        self._before = before
+
+        assert after in (None, 'NFC', 'NFD', 'NFKC', 'NFKD')
+        self._after = after
+
+    def fit(self, texts):
+        return self
+
+    def transform(self, texts):
+        rr = []
+        for text in texts:
+            if self._before:
+                text = unicodedata.normalize(self._before, text)
+            text = aggressively_normalize(text)
+            if self._after:
+                text = unicodedata.normalize(self._after, text)
+            rr.append(text)
+        return rr
+
+    def fit_transform(self, texts):
+        self.fit(texts)
+        return self.transform(texts)
+
+
 class WhitespaceNormalizer(TransformerMixin):
     def fit(self, texts):
         return self
 
-    def fit_transform(self, texts):
+    def transform(self, texts):
         return map(lambda s: u' '.join(s.split()), texts)
 
-    def transform(self, texts):
+    def fit_transform(self, texts):
         return map(lambda s: u' '.join(s.split()), texts)
--- camacho/preprocess/text/tokenizers.py
+++ camacho/preprocess/text/tokenizers.py
@@ -30,16 +30,11 @@ class CodeUnitTokenizer(TransformerMixin):
         return map(list, texts)
 
 
-def segment_raw(text):
+def segments_from_text(text):
     """
-    Don't do any normalization before segmenting text.
+    text -> list of str
 
-    * Useful when you have already done some form of Unicode normalization
-      before you want to segment into characters as-is.
-
-    * Otherwise, a bad choice.
-
-    * Does not group jamo.
+    Does not group jamo.
     """
     nn = map(unicodedata.combining, text)
     buf = []
@@ -56,58 +51,6 @@ def segment_raw(text):
     return map(lambda r: ''.join(r), rr)
 
 
-def segment_nfc(text):
-    """
-    Applies NFC normalization, then segments.
-
-    * Useful if you (a) haven't already normalized, and (b) don't trust
-      segment_aggressive().
-
-    * Does not group jamo.
-    """
-    text = unicodedata.normalize('nfc', text)
-    return segment_raw(text)
-
-
-def reorder_segment(nn_cc):
-    nn_cc.sort()
-    return ''.join(map n, c: c, nn_cc)
-
-
-def segment_aggressive(text):
-    """
-    Aggressively normalizes text before segmentation.
-
-    * Combining characters can interact in a way that inhibits combining,
-      depending on how they are ordered.  So we enforce a canonical order before
-      normalization.
-
-    * Does not group jamo.
-    """
-    # Decompose.
-    text = unicodedata.normalize('nfd', text)
-
-    # Segment.
-    nn = map(unicodedata.combining, text)
-    buf = []
-    nnn_ccc = []
-    for c, n in zip(text, nn):
-        if n:
-            buf.append((n, c))
-            continue
-        if buf:
-            nnn_ccc.append(buf)
-        buf = [(n, c)]
-    if buf:
-        nnn_ccc.append(buf)
-
-    # Reorder decomposed segments in a non-standards-compliant way.
-    ss = map(reorder_segment, nnn_ccc)
-
-    # Now, NFC-normalize that.
-    return map(lambda s: unicodedata.normalize('nfc', s), ss)
-
-
 class CharacterTokenizer(TransformerMixin):
     """
     'Tokenize' text on user-perceived characters.
@@ -115,13 +58,7 @@ class CharacterTokenizer(TransformerMixin):
     Joins Unicode combining characters.  Slower than CodeUnitTokenizer.
     """
 
-    def __init__(self, norm='aggressive'):
-        self._norm = norm
-        self._segment_f = {
-            'raw': segment_raw,
-            'nfc': segment_nfc,
-            'aggressive': segment_aggressive,
-        }[norm]
+    def __init__(self):
         self._fitted = False
 
     def fit(self, texts):
@@ -134,7 +71,7 @@ class CharacterTokenizer(TransformerMixin):
 
     def transform(self, texts):
         assert self._fitted
-        return map(self._segment_f, texts)
+        return map(segments_from_text, texts)
 
 
 class WhitespaceTokenizer(TransformerMixin):
