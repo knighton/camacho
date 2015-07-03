531262b6f70d76bad8fb35d102f9ac66fdb53ec6
James Knighton <iamknighton@gmail.com>
2015-07-02 21:13:22 -0700

    ClassifierMixin.

--- camacho/base.py
+++ camacho/base.py
@@ -33,3 +33,35 @@ class ReversibleTransformerMixin(TransformerMixin):
         transformed items -> items
         """
         raise NotImplementedError
+
+
+class ClassifierMixin(ClassifierMixin):
+    """
+    Used to classify data.
+    """
+
+    def fit(self, X, y):
+        """
+        Trian the model.
+        """
+        raise NotImplementedError
+
+    def predict(self, X):
+        """
+        Predict the class.
+        """
+        raise NotImplementedError
+
+    def classes(self):
+        """
+        Return the classes that it predicts.
+        """
+        raise NotImplementedError
+
+    def predict_proba(self, X):
+        """
+        Return the raw probabilities of each class.
+
+        Column per class (see classes()).
+        """
+        raise NotImplementedError
