c70c4daa6b3d46a85a97562602cd2df7d0618d83
James Knighton <iamknighton@gmail.com>
2015-07-09 17:08:24 -0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(name = 'Camacho',
+      version = '0.0.1',
+      description = 'Hyperoptimizer',
+      author = 'James Knighton',
+      author_email = 'jknighton@livefyre.com',
+      url = 'https://github.com/Livefyre/camacho',
+      license = 'MIT',
+      install_requires = ['nltk', 'keras'],
+      packages = find_packages(),
+)
